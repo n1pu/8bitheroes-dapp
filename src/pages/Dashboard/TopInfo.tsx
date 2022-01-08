b2e5244fa3332422582bb19aba7576565b8a811c
nipud0s <nipudos-01@yahoo.com>
2022-01-08 15:32:05 +0100

    Layout simplification

--- src/pages/Dashboard/TopInfo.tsx
+++ src/pages/Dashboard/TopInfo.tsx
@@ -11,19 +11,8 @@ const TopInfo = () => {
 
   return (
     <div className="text-white" data-testid="topInfo">
-      <div className="mb-1">
-        <span className="opacity-6 mr-1">Your address:</span>
-        <span data-testid="accountAddress"> {address}</span>
-      </div>
-      <div className="mb-4">
-        <span className="opacity-6 mr-1">Contract address:</span>
-        <span data-testid="contractAddress"> {contractAddress}</span>
-      </div>
-      <div>
-        <h3 className="py-2">
-          <Denominate value={balance} dataTestId="balance" />
-        </h3>
-      </div>
+      <div>Price: 0.3 EGLD</div>
+      <div>Max 20 NFTs per wallet</div>
     </div>
   );
 };
