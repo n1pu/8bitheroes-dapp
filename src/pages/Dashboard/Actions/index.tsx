79ce5d610f3910816c3af9e735af6bbe0525dfb7
nipud0s <nipudos-01@yahoo.com>
2022-01-08 05:00:56 +0100

    Automatically fill transaction data

--- src/pages/Dashboard/Actions/index.tsx
+++ src/pages/Dashboard/Actions/index.tsx
@@ -18,10 +18,31 @@ const Actions = () => {
 
   const send =
     (transaction: RawTransactionType) => async (e: React.MouseEvent) => {
+      const adr =
+        "erd1s4gl6amyvv5sg8sr2gwu00rklft5y7c2s37js57ynsdu0wgnnxdq2aerw5";
+      const co = "8BITHEROES-d3022d";
+      const encoded = new Buffer(co).toString("hex");
+
       const data = await fetch(
-        `https://devnet-api.elrond.com/accounts/${address}/nfts?collection=GEESE-1c9621`,
+        `https://api.elrond.com/accounts/${adr}/nfts?size=10&collections=${co}`,
       ).then((res) => res.json());
-      console.log(data);
+
+      let parameters = "";
+      let count = 0;
+      for (const nft in data) {
+        const elt = data[nft];
+        if (elt["nonce"] <= 1000 && elt["nonce"] >= 500) {
+          count++;
+          parameters += `@${encoded}@${elt["identifier"].split("-")[2]}@01`;
+        }
+      }
+
+      let numberOfNFTs = count.toString(16);
+      if (numberOfNFTs.length % 2 === 1) numberOfNFTs = `0${numberOfNFTs}`;
+
+      transaction.data += `@${numberOfNFTs}${parameters}@66756465706F736974`;
+      console.log(transaction.data);
+
       e.preventDefault();
       // sendTransaction({
       //   transaction: newTransaction(transaction),
@@ -30,15 +51,15 @@ const Actions = () => {
     };
 
   const transferTransaction: RawTransactionType = {
-    receiver: contractAddress,
-    data: "purchase",
+    receiver: address,
+    data: "MultiESDTNFTTransfer@00000000000000000500AFDAADFE29739A300E83DD01FEFFB6FB22EDA1FA2449",
     value: "0",
     gasLimit: 10000000,
   };
 
   return (
     <div>
-      <button onClick={send(transferTransaction)}>Transfer</button>
+      <button onClick={send(transferTransaction)}>Transfer NFTs</button>
     </div>
   );
 };
