056be5ba42a1fb91a0d513dc11dd43533b343fc6
nipud0s <nipudos-01@yahoo.com>
2022-01-08 05:00:21 +0100

    Remove transactions list

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,54 +1,9 @@
 import * as React from "react";
-import * as Dapp from "@elrondnetwork/dapp";
-import { faBan } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import PageState from "components/PageState";
-import { contractAddress } from "config";
-import { useContext, useDispatch } from "context";
 import Actions from "./Actions";
-import { getTransactions } from "./helpers/asyncRequests";
 import TopInfo from "./TopInfo";
-import Transactions from "./Transactions";
 
 const Dashboard = () => {
   const ref = React.useRef(null);
-  const { apiAddress, address } = Dapp.useContext();
-  const { transactionsFetched } = useContext();
-  const dispatch = useDispatch();
-
-  const fetchData = () => {
-    getTransactions({
-      apiAddress,
-      address,
-      timeout: 3000,
-      contractAddress,
-    }).then(({ data, success }) => {
-      dispatch({
-        type: "setTransactions",
-        transactions: data,
-        transactionsFetched: success,
-      });
-    });
-  };
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  React.useEffect(fetchData, []);
-
-  if (transactionsFetched === undefined) {
-    return <PageState svgComponent={<></>} spin />;
-  }
-
-  if (transactionsFetched === false) {
-    return (
-      <PageState
-        svgComponent={
-          <FontAwesomeIcon icon={faBan} className="text-secondary fa-3x" />
-        }
-        title="Unavailable"
-        className="dapp-icon icon-medium"
-      />
-    );
-  }
 
   return (
     <div className="container py-4" ref={ref}>
@@ -62,7 +17,6 @@ const Dashboard = () => {
                   <Actions />
                 </div>
               </div>
-              <Transactions />
             </div>
           </div>
         </div>
